465aca9bfc3c7a9067f387a47439bc4af4f67ef4
Matt <m18taylor@gmail.com>
2019-10-19 12:07:31 -0400

    Clean App

--- lit-up/src/App.js
+++ lit-up/src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import Button from '@material-ui/core/Button'
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
 
 import Navigation from './Navigation/Navigation'
 import Gallery from './Gallery/Gallery'
 import DynamicChart from './DynamicChart/DynamicChart'
 import './App.css';
-
-const options = {
-  title: {
-    text: 'My chart'
-  },
-  series: [{
-    data: [1, 2, 3]
-  }]
-}
 
 
 function App() {
@@ -26,11 +15,6 @@ function App() {
         <Gallery />
         <DynamicChart />
         <Button variant="contained" color="primary">Hello Tyler</Button>
-
-        <HighchartsReact
-          highcharts={Highcharts}
-          options={options}
-        />
     </div>
   );
 }
--- lit-up/src/DynamicChart/DynamicChart.js
+++ lit-up/src/DynamicChart/DynamicChart.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
 import styles from'./DynamicCharts.module.css'
 
+const options = {
+    title: {
+      text: 'My chart'
+    },
+    series: [{
+      data: [1, 2, 3]
+    }]
+  }
+
 export default function DynamicCharts(props) {
-    return <p>DynamicCharts</p>
+    return         <HighchartsReact
+                    highcharts={Highcharts}
+                    options={options}
+                />
+
 }
